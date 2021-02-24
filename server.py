936235785c80aac871d07985735427b2b59cafe3
1seok2 <misohaha31@naver.com>
2021-02-24 21:38:39 +0900

    add flask

--- server.py
+++ server.py
@@ -1,7 +1,26 @@
 import os
-
 from config.firebase import db
+from flask import Flask, render_template
 
-print("-" * 60)
+# data = {
+#     "age": 24,
+#     "name": "choi",
+#     "date": "2020.12.03"
+# }
 
-db.child("name").push({"company": "google"})
+# db.child("name").child("name").update(data)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    print("-" * 60)
+    print("  server is start")
+    print("-" * 60)
+
+    app.run(debug=True)
