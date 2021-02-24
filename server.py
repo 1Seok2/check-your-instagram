600ba64c765439757b0897057ac2875226f772c3
1seok2 <misohaha31@naver.com>
2021-02-24 21:57:00 +0900

    add 404 route

--- server.py
+++ server.py
@@ -13,6 +13,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html')
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
