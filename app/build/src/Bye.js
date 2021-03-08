4fab06cd38a6b20d30e4d507c0a6d73252918e8b
최원석3 [goesnow] <goesnow@nexon.co.kr>
2021-03-08 16:32:10 +0900

    some change

---
+++ app/build/src/Bye.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Bye = void 0;
+const Bye = () => console.log('Bye!');
+exports.Bye = Bye;
+//# sourceMappingURL=Bye.js.map
