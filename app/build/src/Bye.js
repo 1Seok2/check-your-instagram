c1e20916c19203cab45df53fa19f213f968f6f12
최원석3 [goesnow] <goesnow@nexon.co.kr>
2021-03-08 17:31:15 +0900

    remove build files

--- app/build/src/Bye.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Bye = void 0;
-const Bye = () => console.log('Bye!');
-exports.Bye = Bye;
-//# sourceMappingURL=Bye.js.map
